9550c617fddf33b22dc002e8119688203c7430bf
Peter Pfläging <peter@pflaeging.net>
2019-11-28 20:48:57 +0100

    dashboard login update

--- dashboard-login-infos.sh
+++ dashboard-login-infos.sh
@@ -3,7 +3,8 @@
 # get nodePort port
 PORT=`kubectl get svc kubernetes-dashboard -n kubernetes-dashboard -o yaml | grep '\- nodePort:'| cut -d " " -f5`
 # get ip address from internal interface (only works with virtualbox config!)
-IP=`ip route | grep enp0s8 | cut -d " " -f 9`
+# IP=`ip route | grep enp0s8 | cut -d " " -f 9`
+IP=`kubectl describe node gubernat.pflaeging.net | grep InternalIP | cut -d: -f2| sed 's/ //g'`
 # get token for pre-defined admin-user 
 TOKEN=`kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep admin-user | awk '{print $1}')| grep "^token:" | cut -d: -f2`
 
