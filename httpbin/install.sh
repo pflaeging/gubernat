bb53003aa4c09ccf2aeac6f5687b86d3d18f3fc3
Peter Pflaeging <peter@pflaeging.net>
2023-03-23 11:52:07 +0100

    update doc, better configs, test with latest alma

---
+++ httpbin/install.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+. ../env.sh
+echo '# Don't edit this, it's generated from kustomization-template.yaml by ./install.sh' > kustomization.yaml
+sed "s/i-am.replaced.by.install.sh/$WILDCARD_INGRESS/g" < kustomization-template.yaml >> kustomization.yaml
+
+#kubectl apply -k .
