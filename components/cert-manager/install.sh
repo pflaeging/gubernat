a8445080563b53f971ff36a255b67aef05f92506
Peter Pfläging <peter@pflaeging.net>
2024-07-19 15:24:48 +0200

    Add trustmanager for offline use

--- components/cert-manager/install.sh
+++ components/cert-manager/install.sh
@@ -2,7 +2,10 @@
 
 kubectl apply -k .
 kubectl wait pod -l app=webhook --for=condition=Ready -n cert-manager --timeout=500s
+kubectl apply -f trustmanager.yaml -n cert-manager
 kubectl apply -f gubernat-ca.yaml -n cert-manager
+kubectl wait pod -l app=trust-manager --for=condition=Ready -n cert-manager --timeout=500s
+kubectl apply -f trust-bundle.yaml -n cert-manager
 {% if cert_manager_admin_mail is defined %}
 kubectl apply -f letsencrypt.yaml -n cert-manager
 {% endif %}
