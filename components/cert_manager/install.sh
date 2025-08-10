38ea81f863e44a82b19e75d15b2c9afac3ce583b
abilnf <slin.lbeog@gmail.com>
2025-08-10 18:11:07 +0200

    convert component names to underscores to fix ansible variables

--- components/cert_manager/install.sh
+++ components/cert_manager/install.sh
@@ -3,6 +3,6 @@
 kubectl apply -k .
 kubectl wait pod -l app=webhook --for=condition=Ready -n cert-manager --timeout=500s
 kubectl apply -f gubernat-ca.yaml -n cert-manager
-{% if cert-manager_config.admin_mail is defined %}
+{% if cert_manager_config.admin_mail is defined %}
 kubectl apply -f letsencrypt.yaml -n cert-manager
 {% endif %}
